50dc087ec3fa708c7953229a3a68846e1f76f1da
Peter Nelson <git@ppffnn.com>
2018-12-04 15:28:42 -0800

    Started Skinning

--- Zuma/ZumaInstallationBeginningViewController.swift
+++ Zuma/ZumaInstallationBeginningViewController.swift
@@ -15,6 +15,7 @@ class ZumaInstallationBeginningViewController: UIViewController {
 
         // Do any additional setup after loading the view.
         setupNavBar()
+        setupButtonNavigation()
     }
     
     func setupNavBar() {
@@ -25,6 +26,63 @@ class ZumaInstallationBeginningViewController: UIViewController {
         // Hide the back button
         let backButton = UIBarButtonItem(title: "", style: .plain, target: navigationController, action: nil)
         navigationItem.leftBarButtonItem = backButton
+    }
+    
+    func setupButtonNavigation() {
+        
+
+        let guideButton : UIView = self.view.viewWithTag(1)! as UIView
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(guideNormalTap(_:)))
+        tapGesture.numberOfTapsRequired = 1
+        guideButton.addGestureRecognizer(tapGesture)
+            
+        let longGesture = UILongPressGestureRecognizer(target: self, action: #selector(guideLongTap(_:)))
+        guideButton.addGestureRecognizer(longGesture)
+        
+        
+        let installDoneButton : UIView = self.view.viewWithTag(2)! as UIView
+        
+        let tapGesture2 = UITapGestureRecognizer(target: self, action: #selector(installDoneNormalTap(_:)))
+        tapGesture.numberOfTapsRequired = 1
+        installDoneButton.addGestureRecognizer(tapGesture2)
+        
+        let longGesture2 = UILongPressGestureRecognizer(target: self, action: #selector(installDoneLongTap(_:)))
+        installDoneButton.addGestureRecognizer(longGesture2)
+
+    }
+    
+    
+    @objc func guideNormalTap(_ sender: UIGestureRecognizer){
+        //print("Normal tap")
+        self.performSegue(withIdentifier:"InstallationGuideSegue", sender: sender)
+    }
+    
+    @objc func guideLongTap(_ sender: UIGestureRecognizer){
+        
+        if sender.state == .ended {
+            //print("LongTap Ended")
+            self.performSegue(withIdentifier:"InstallationGuideSegue", sender: sender)
+        }
+        //else if sender.state == .began {
+        //print("LongTap Started")
+        //}
+    }
+    
+    @objc func installDoneNormalTap(_ sender: UIGestureRecognizer){
+        //print("Normal tap")
+        self.performSegue(withIdentifier:"SetupZumasSegue", sender: sender)
+    }
+    
+    @objc func installDoneLongTap(_ sender: UIGestureRecognizer){
+        
+        if sender.state == .ended {
+            //print("LongTap Ended")
+            self.performSegue(withIdentifier:"SetupZumasSegue", sender: sender)
+        }
+        //else if sender.state == .began {
+        //print("LongTap Started")
+        //}
     }
 
     /*
